450a6b3ad4c47fad476ad834312491c149723435
Sergey Fomenko <fomenko.personal@gmail.com>
2017-06-30 01:14:52 +0300

    added project structure

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+"use strict";
+
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const debug = require("gulp-debug");
+const del = require("del");
+const newer = require("gulp-newer");
+const cssnano = require("gulp-cssnano");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const rename = require("gulp-rename");
+const gulpIf = require("gulp-if");
+const browserSync = require("browser-sync").create();
+
+// Запуск `NODE_ENV=production gulp [задача]` приведет к сборке без sourcemaps
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == "development";
+
+gulp.task("styles", function() {
+
+  return gulp.src("src/style.scss")
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        "> 1%",
+        "last 2 versions",
+        "Firefox ESR"
+      ]})
+    ]))
+    .pipe(cssnano())
+    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("markup", function() {
+  return gulp.src("src/**/*.html", {since: gulp.lastRun("markup")})
+    .pipe(newer("build"))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("images", function() {
+  return gulp.src("src/blocks/**/*.{png,jpg,svg}", {since: gulp.lastRun("images")})
+    .pipe(newer("build"))
+    .pipe(rename(function(path) {
+      path.dirname = "";
+      return path;
+    }))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("video", function() {
+  return gulp.src("src/blocks/**/*.{mp4,flv,ogv,webm,avi}", {since: gulp.lastRun("video")})
+    .pipe(newer("build"))
+    .pipe(rename(function(path) {
+      path.dirname = "";
+      return path;
+    }))
+    .pipe(gulp.dest("build/video"));
+});
+
+gulp.task("assets", function() {
+  return gulp.src("src/assets/**/*.*")
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  gulp.parallel("styles", "markup", "images", "video", "assets"))
+);
+
+gulp.task("watch", function() {
+  gulp.watch("src/blocks/**/*.*", gulp.series("styles"));
+
+  gulp.watch("src/**/*.html", gulp.series("markup"));
+
+  gulp.watch("src/blocks/**/*.{png,jpg,svg}", gulp.series("images"));
+});
+
+gulp.task("serve", function() {
+  browserSync.init({
+    server: "build"
+  });
+
+  browserSync.watch("build/**/*.*").on("change", browserSync.reload);
+});
+
+gulp.task("dev",
+  gulp.series("build", gulp.parallel("watch", "serve"))
+);
